26af8e198e4602d1800ecbe120333bf638aa2b26
Matt Burton <mattwburton@gmail.com>
2022-01-04 16:27:15 +0000

    server.js and user.routes.js filled out

---
+++ src/server.js
@@ -0,0 +1,14 @@
+requestAnimationFrame("./db/connection");
+const express = require("express");
+const cors = require("cors");
+const userRouter = require("./user/user.routes");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cores());
+app.use(userRouter);
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
