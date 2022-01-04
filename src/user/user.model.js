341b1390bde8940b5d7637b763d88b3bff9ccbbf
DavidALevy <david.levy.levy@btinternet.com>
2022-01-04 17:24:43 +0000

    userModel added

---
+++ src/user/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+\@.+\..+/,
+  },
+  pass: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
